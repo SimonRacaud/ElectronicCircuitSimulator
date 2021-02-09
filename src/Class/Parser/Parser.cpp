feed7660333a9a9b87e946b429abfaf16de5e29d
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-02-09 11:10:23 +0100

    - Add Parser V2

--- src/Class/Parser/Parser.cpp
+++ src/Class/Parser/Parser.cpp
@@ -7,76 +7,6 @@
 
 #include "Parser.hpp"
 
-static bool strIsNum(const std::string &str)
-{
-    for (char const &c : str)
-        if (c >= '0' && c <= '9')
-            return false;
-    return true;
-}
-
-void nts::Parser::parsingFile(const std::string &filepath, nts::Circuit *dest)
-{
-    std::list<std::string> file = nts::Parser::readFile(filepath);
-    std::list<std::string>::iterator links = std::find_if(file.begin(),
-    file.end(), [&](std::string str){ return str == LINKS;});
-    std::list<std::string>::iterator chipsets = std::find_if(file.begin(),
-    file.end(), [&](std::string str){ return str == CHIPSETS;});
-    if (links == file.end() || chipsets == file.end())
-        throw ParsingError("Parsing", "Wrong format");
-    chipsets++;
-    std::map<std::string, ssize_t> map_input = nts::Parser::loadSinceName(INPUT, chipsets, links);
-    std::map<std::string, ssize_t> map_output = nts::Parser::loadSinceName(OUTPUT, chipsets, links);
-    //std::cout << "input: " << map_input.size() << std::endl;
-    //std::cout << "output: " << map_output.size() << std::endl;
-    if (map_output.size() + map_input.size() + 1 != (size_t) std::distance(chipsets, links))
-        throw ParsingError("Parsing", "too much line in chipset");
-    links++;
-    nts::Parser::loadLinks(links, file.end(), map_input, map_output);
-    for (const auto& m : map_input) {
-        std::cout << m.first << " = " << m.second << ";" << std::endl;
-    }
-    for (const auto& m : map_output) {
-        std::cout << m.first << " = " << m.second << ";" << std::endl;
-    }
-}
-
-void nts::Parser::loadLinks(
-std::list<std::string>::iterator start,
-std::list<std::string>::iterator end,
-std::map<std::string, ssize_t> &map_input,
-std::map<std::string, ssize_t> &map_output)
-{
-    std::string key;
-    std::string key_next;
-    size_t found;
-    size_t found_next;
-
-    for (std::list<std::string>::iterator it = start; it != end; it++) {
-        found = (*it).find(TOKEN);
-        found_next = (found != std::string::npos) ? (*it).find(" gate:", found + 1) + 6 : std::string::npos;
-        if (found != std::string::npos && found_next != std::string::npos) {
-            std::cout << (*it).substr(0, found) << " in " << (*it) << std::endl;
-            key = (*it).substr(0, found);
-            key_next = (*it).substr(found_next, (*it).length());
-            std::cout << "next: '" << key_next << "'" << " in " << (*it) << std::endl;
-            if (strIsNum(key_next))
-                throw ParsingError("Parsing", "Key is not Num");
-            if (map_input.find(key) != map_input.end()) {
-                //std::cout << "input: " << key << std::endl;
-                map_input[key] = atoi(key_next.c_str());
-            } else if (map_output.find(key) != map_output.end()) {
-                //std::cout << "output: " << key << std::endl;
-                map_output[key] = atoi(key_next.c_str());
-            } else {
-                throw ParsingError("Parsing", "Variable name not define");
-            }
-        } else {
-            throw ParsingError("Parsing", "no parser Token");
-        }
-    }
-}
-
 void nts::Parser::cleanComment(std::list<std::string> &file)
 {
     std::list<std::string>::iterator tmp;
@@ -114,19 +44,44 @@ std::list<std::string> nts::Parser::readFile(const std::string &filepath)
     return all_file;
 }
 
-std::map<std::string, ssize_t> nts::Parser::loadSinceName(std::string key,
-std::list<std::string>::iterator start, std::list<std::string>::iterator end)
+void nts::Parser::parsingFile(const std::string &filepath, nts::Circuit *dest)
 {
-    std::map<std::string, ssize_t> links;
-    std::string tmp;
+    std::list<std::string> file = nts::Parser::readFile(filepath);
+    std::list<std::string>::iterator links = std::find_if(file.begin(),
+    file.end(), [&](std::string str){ return str == LINKS;});
+    std::list<std::string>::iterator chipsets = std::find_if(file.begin(),
+    file.end(), [&](std::string str){ return str == CHIPSETS;});
+
+    if (links == file.end() || chipsets == file.end())
+        throw ParsingError("Parsing", "Wrong format");
+    chipsets++;
+    std::map<std::string, std::string> mapChipsets = nts::Parser::cutAt(' ', chipsets, links);
+    links++;
+    std::map<std::string, std::string> mapLinks = nts::Parser::cutAt(' ', links, file.end());
+    //std::tuple<std::string, std::string, std::string, std::string> nts::Parser::cleanLink(mapLinks, mapChipsets);
+    /*for (const auto& m : mapChipsets) {
+        std::cout << m.first << " = " << m.second << ";" << std::endl;
+    }
+    for (const auto& m : mapLinks) {
+        std::cout << m.first << " = " << m.second << ";" << std::endl;
+    }
+    std::cout << mapChipsets.size() << "|" << mapLinks.size() << std::endl;*/
+}
+
+std::map<std::string, std::string> nts::Parser::cutAt(const char c, std::list<std::string>::iterator start, std::list<std::string>::iterator end)
+{
     size_t found;
+    std::map<std::string, std::string> map;
 
     for (std::list<std::string>::iterator it = start; it != end; it++) {
-        found = (*it).find(key);
+        found = (*it).find(c);
         if (found != std::string::npos) {
-            tmp = (*it).erase(0, key.length());
-            links.insert({tmp, -1});
+            //std::cout << (*it).substr(0, found) << "|" << (*it).substr(found + 1, (*it).length()) << std::endl;
+            //map[(*it).substr(0, found)] = (*it).substr(found, (*it).length());
+            map[(*it).substr(found, (*it).length())] = (*it).substr(0, found);
+        } else {
+            throw ParsingError("Parsing", "Token not found");
         }
     }
-    return links;
+    return map;
 }
--- src/Class/Parser/Parser.hpp
+++ src/Class/Parser/Parser.hpp
@@ -10,6 +10,7 @@
 
 #include <map>
 #include <list>
+#include <tuple>
 #include <fstream>
 #include <algorithm>
 #include "Class/Exception/ParsingError.hpp"
@@ -32,14 +33,7 @@ namespace nts
         private :
             static std::list<std::string> readFile(const std::string &filepath);
             static void cleanComment(std::list<std::string> &file);
-            static void loadLinks(
-                std::list<std::string>::iterator start,
-                std::list<std::string>::iterator end,
-                std::map<std::string, ssize_t> &map_input,
-                std::map<std::string, ssize_t> &map_output);
-            static std::map<std::string, ssize_t> loadSinceName(std::string key,
-                std::list<std::string>::iterator start,
-                std::list<std::string>::iterator end);
+            static std::map<std::string, std::string> cutAt(const char c, std::list<std::string>::iterator start, std::list<std::string>::iterator end);
     };
 }
 
